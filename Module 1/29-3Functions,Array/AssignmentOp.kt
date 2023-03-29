d47027253fe95e54d9e6635fb53f53db75c56034
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-03-29 14:35:41 +0530

    Add files via upload

---
+++ Module 1/29-3Functions,Array/AssignmentOp.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+
+    var a = 6
+    var b = 5
+
+    a+=b//a=a+b
+    println(a)//11
+
+    a-=b//a=a-b
+    println(a)//6
+
+    a*=b//a=a*b
+    println(a)//30
+
+    a/=b//a=a/b
+    println(a)//6
+}
